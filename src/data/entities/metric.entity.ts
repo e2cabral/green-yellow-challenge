aebbc5939d1fc54e9725b3bb7ef2a4286120db80
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-09-25 18:43:16 -0300

    chore(entities): create a metric entity

---
+++ src/data/entities/metric.entity.ts
@@ -0,0 +1,16 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('metric')
+export class MetricEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'metric_id' })
+    metricId: string;
+
+    @Column({ name: 'date_time' })
+    dateTime: string;
+
+    @Column({ name: 'value' })
+    value: string;
+}
