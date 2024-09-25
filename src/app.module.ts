1971e7f8615b72821a86a205b058f0b9ab1888e6
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-09-25 17:40:18 -0300

    feat(database): add a database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PresentationModule } from './presentation/presentation.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [PresentationModule],
+  imports: [
+    PresentationModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "password",
+      database: "green_yellow",
+      synchronize: true,
+      logging: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
